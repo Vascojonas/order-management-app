70a6d31fac081b759dd6abe1a5c5d8adebb52217
Vasco Jonas Mabui <vascojonas.j@gmail.com>
2022-07-18 23:08:25 +0700

    .

--- resources/js/components/app.js
+++ resources/js/components/app.js
@@ -108,7 +108,7 @@ function App(){
                 <Route path='/admin/produtos/cadastrar/:id' element={<Login/>}/>
                 <Route path='/admin/produtos/listar' element={<Login/>}/>
                 <Route path='/admin/funcionarios' exact element={<Login/>}/>
-                <Route path='/admin/funcionarios/cadastrar'  element={<Login/>}/>
+                <Route path='/admin/funcionarios/cadastrar'  element={<FuncionarioCadastro/>}/>
               <Route path='/admin/produtos/encomendas'  element={<Login/>}/>
 
       
